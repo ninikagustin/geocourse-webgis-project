15a9ec63709f316da072f56762e3671736ca3341
ninikagustin <ninik.agustin@gmail.com>
2023-10-28 11:58:21 +0700

    Step 5 - Membuat Form Tambah Fasilitas

--- project/project/settings.py
+++ project/project/settings.py
@@ -45,6 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     
+    # Leaflet
+    'leaflet',
+    
+    # crips
+    "crispy_forms",
+    "crispy_bootstrap5",
+    
     # Custom Apps
     'bikini_bottom'
 ]
@@ -151,3 +158,16 @@ if os.name == 'nt':
         VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
     os.environ['PROJ_LIB'] = os.path.join(
         VENV_BASE, 'Lib\\site-packages\\osgeo\\data\\proj') + ';' + os.environ['PATH']
+    
+    # LEAFLET SETTING
+    LEAFLET_CONFIG = {
+    'DEFAULT_CENTER': [11.608274122747922, 165.37697796900906],
+    'DEFAULT_ZOOM': 11,
+    'MIN_ZOOM': 3,
+    'MAX_ZOOM': 18,
+    'TILES': 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    }
+    
+    # CRISPY FORM SETTINGS
+    CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+    CRISPY_TEMPLATE_PACK = "bootstrap5"
