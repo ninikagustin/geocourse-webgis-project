f0b93ff1065bbdd07c0d1c42abe825c5ffa63c24
ninikagustin <ninik.agustin@gmail.com>
2023-11-05 09:45:54 +0700

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,10 +26,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY') == 'True'
+SECRET_KEY = env('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = env('HOSTS').split(',')
 
@@ -172,6 +172,6 @@ if os.name == 'nt':
     'TILES': 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     }
     
-    # CRISPY FORM SETTINGS
-    CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-    CRISPY_TEMPLATE_PACK = "bootstrap5"
+# CRISPY FORM SETTINGS
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
